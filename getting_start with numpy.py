0bbcb890627443d94d58472ba9f600089ded55c8
Lipi Kumari <47522378+lipikumari@users.noreply.github.com>
2021-02-08 02:29:11 +0530

    Add files via upload

---
+++ getting_start with numpy.py
@@ -0,0 +1,11 @@
+import numpy
+arr=numpy.array([12,45,89,78])
+print(arr)
+#NUMPY can aslo be referred as np instead of numpy
+import numpy as np
+my_array=[66,78,67,231,23]
+arr=np.array(my_array)
+print(arr)
+#checking Numpy version
+import numpy as np
+print(np._version_)
