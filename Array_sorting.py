69de55b60a13f8ff1250329db0a20ea470869c75
Lipi Kumari <47522378+lipikumari@users.noreply.github.com>
2021-03-24 20:29:21 +0530

    Add files via upload

---
+++ Array_sorting.py
@@ -0,0 +1,25 @@
+#Numpy Array sorting
+#sorting means putting elements in an ordered sequence
+#sorting arrays
+#Example 1
+import numpy as np
+arr=np.array([2,5,6,3,9,8,7])
+x=np.sort(arr)
+print(x)
+#example 2
+import numpy as np
+arr=np.array([2,3,4,5,9,1,8,7,10])
+x=np.sort(arr)
+print(x)
+#Example 3
+import numpy as np
+arr=np.array(['banana','cherry','mango','litchi'])
+print(np.sort(arr))
+#Example 4
+import numpy as np
+arr=np.array([True,False,True])
+print(np.sort(arr))
+#sorting 2-D array
+import numpy as np
+arr=np.array([[3,2,4],[5,0,1]])
+print(np.sort(arr))
